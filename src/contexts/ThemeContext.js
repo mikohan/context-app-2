aa0a5d1a963ff7693ad879fe6f86bddcc23ce534
mikohan <angara99@gmail.com>
2020-10-07 16:35:15 +0300

    Clean up themeContext

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -4,18 +4,7 @@ import useToggleState from '../hooks/useToggleState';
 export const ThemeContext = createContext();
 
 function ThemeProvider(props) {
-  // constructor(props) {
-  //   super();
-  //   this.state = {
-  //     isDarkMode: true,
-  //   };
-  // }
-
-  const [isDarkMode, toggleTheme] = useToggleState(true);
-
-  // const toggleTheme = () => {
-  //   this.setState({ isDarkMode: !this.state.isDarkMode });
-  // };
+  const [isDarkMode, toggleTheme] = useToggleState(false);
 
   return (
     <ThemeContext.Provider value={{ isDarkMode, toggleTheme: toggleTheme }}>
